76591f37c1488b7e469aa4c636494c18ef57a713
Lakshay yadav <lakshayyadav1010@gmail.com>
2025-07-15 00:21:02 +0530

    added js concepts

---
+++ JS-CONCEPTS/script.js
@@ -0,0 +1,29 @@
+//   const h1 = document.querySelector("h1");
+
+//   h1.style.position = "absolute";
+//   h1.style.top = "50%";
+//   h1.style.left = "50%";
+//   h1.style.transform = "translate(-50%, -50%)";
+//   h1.style.fontSize = "90px";
+
+//  h1.addEventListener("click" , function (){
+//   h1.style.backgroundColor= "red";
+//   h1.style.color= "blue";
+//   h1.innerText="kishangarh";
+// h1.style.textBox = "10px";
+// h1.style.border = "2px solid black";
+// h1.style.borderRadius = "500px";
+// h1.style.textAlign = "center";
+// h1.style.fontSize= "100px";
+//  })
+
+
+const input = document.querySelector("select");
+input.addEventListener("change", function (event) {
+  if (event.target.value !== "") {
+    h1.textContent = `Device selected: ${event.target.value}`;
+  } else {
+    h1.textContent = "Select your device";
+  }
+  console.log(event.target.value);
+});
